4ef9ee9d14923a6b8e673ad920c2722bd9cef627
Charles Fries <charliefries@icloud.com>
2015-02-17 13:49:13 -0800

    Mods

--- discover.php
+++ discover.php
@@ -85,11 +85,15 @@
 				</ul>
 			</div>
 			
-			<br>
+			<br><br>
 			
 			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/181305899&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 			
 			<br>
+			
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/170339902&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+			
+			<br><br>
 			
 			<ol class="breadcrumb">
 				<li><a href="index.php">Home</a></li>
