ae9efc389df2d4553283edbf994dea2e8e0f32a8
Charles Fries <charliefries@icloud.com>
2015-02-17 15:06:36 -0800

    Mods

--- account.php
+++ account.php
@@ -85,7 +85,7 @@
 			<p>Username: charlesfries <a href="#">(Edit)</a></p>
 			<p>Password: <a href="#">Change</a></p>
 			
-			<button class="btn btn-default" type="submit" onclick="signOut()">Sign Out</button>
+			<button class="btn btn-default" type="button" onclick="signOut()">Sign Out</button>
 			
 			<script>
 				function signOut() {
@@ -97,11 +97,27 @@
 			<br><br>
 			
 			<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  Launch demo modal
-</button>
+			<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#myModal">Delete Account</button>
 			
-			<a class="btn btn-danger btn-xs" href="#" role="button">Delete Account</a>
+			<!-- Delete Account Modal -->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+						</div>
+						<div class="modal-body">
+							...
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-primary">Save changes</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 			<br><br>
 			
 			<!-- Breadcrumb -->
@@ -115,25 +131,6 @@
 			<footer>
 				<p>&copy; 2015 Figure Inc.</p>
 			</footer>
-		</div>
-		
-		<!-- Modal -->
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">Modal title</h4>
-					</div>
-					<div class="modal-body">
-						...
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
-					</div>
-				</div>
-			</div>
 		</div>
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
