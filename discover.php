69f25d97915f4123b166752475690a655c7af874
Charles Fries <charliefries@icloud.com>
2015-02-17 16:27:25 -0800

    Mod images

--- discover.php
+++ discover.php
@@ -75,44 +75,48 @@
 			</div>
 		</nav>
 		
+		<!-- Carousel -->
 		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
- 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="http://placehold.it/1200x315" alt="...">
-      <div class="carousel-caption">
-          <h3>Caption Text</h3>
-      </div>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/1200x315" alt="...">
-      <div class="carousel-caption">
-          <h3>Caption Text</h3>
-      </div>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/1200x315" alt="...">
-      <div class="carousel-caption">
-          <h3>Caption Text</h3>
-      </div>
-    </div>
-  </div>
- 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
-</div> <!-- Carousel -->
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			</ol>
+			
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="//placehold.it/1400x500">
+					<div class="carousel-caption">
+						<h3>Caption Text</h3>
+						<p>Subtitle</p>
+					</div>
+				</div>
+				<div class="item">
+					<img src="//placehold.it/1400x500">
+					<div class="carousel-caption">
+						<h3>Caption Text</h3>
+						<p>Subtitle</p>
+					</div>
+				</div>
+				<div class="item">
+					<img src="//placehold.it/1400x500">
+					<div class="carousel-caption">
+						<h3>Caption Text</h3>
+						<p>Subtitle</p>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Controls -->
+			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
 		
 		<!-- Content -->
 		<div class="container">
